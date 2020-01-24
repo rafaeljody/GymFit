bafc18f9aa2a6757bb83e8b59e39569c09257460
fazaazizi <faza.azizi.faz@gmail.com>
2020-01-25 04:49:55 +0700

    gabungan

--- app/src/main/java/com/example/myapplication/PilihTambahActivity.java
+++ app/src/main/java/com/example/myapplication/PilihTambahActivity.java
@@ -2,7 +2,9 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class PilihTambahActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class PilihTambahActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pilih_tambah);
+    }
+
+    public void formgym(View view){
+        Intent intent = new Intent(PilihTambahActivity.this, FormTambahGymActivity.class);
+        startActivity(intent);
+    }
+
+    public void formtrainer(View view){
+        Intent intent = new Intent(PilihTambahActivity.this, FormTambahTrainerActivity.class);
+        startActivity(intent);
     }
 }
