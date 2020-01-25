1200835c994ef7d8b1030a37ccc7f25098faeb9b
fazaazizi <faza.azizi.faz@gmail.com>
2020-01-26 00:34:15 +0700

    jkndbjkbnfefbcq3

--- app/src/main/java/com/example/myapplication/FormTambahTrainerActivity.java
+++ app/src/main/java/com/example/myapplication/FormTambahTrainerActivity.java
@@ -1,20 +1,38 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
 
 public class FormTambahTrainerActivity extends AppCompatActivity {
+
+    Button ch,up;
+    ImageView img;
+    StorageReference mStorageReference;
+    public Uri imguri;
+    private StorageTask uploadTask;
 
     DatabaseReference database; // untuk mengambil root pada database
 
@@ -29,6 +47,30 @@ public class FormTambahTrainerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_tambah_trainer);
+
+        mStorageReference= FirebaseStorage.getInstance().getReference("profile_trainer");
+
+        ch=(Button)findViewById(R.id.buttonChoose);
+        up=(Button)findViewById(R.id.btn_UploadTrainer);
+        img=(ImageView)findViewById(R.id.imageView);
+        ch.setOnClickListener(new View.OnClickListener(){
+                                  @Override
+                                  public void onClick(View view){
+                                      Filechoser();
+                                  }
+                              }
+        );
+        up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(uploadTask!=null && uploadTask.isInProgress()){
+                    Toast.makeText(FormTambahTrainerActivity.this, "upload in progress",Toast.LENGTH_LONG).show();
+                }else{
+                    FileUploader();
+                }
+
+            }
+        });
 
         //Inisialisasi Data Trainer
         etNamaTrainer   = (EditText) findViewById(R.id.edt_NamaTrainer);
@@ -67,6 +109,51 @@ public class FormTambahTrainerActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private String getExtension(Uri uri){
+        ContentResolver cr=getContentResolver();
+        MimeTypeMap mimeTypeMap=MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(cr.getType(uri));
+    }
+
+    private void FileUploader(){
+        StorageReference ref=mStorageReference.child(System.currentTimeMillis()+"."+getExtension(imguri));
+
+        uploadTask= ref.putFile(imguri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // Get a URL to the uploaded content
+                        //Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                        Toast.makeText(FormTambahTrainerActivity.this, "image uploaded succesfully",Toast.LENGTH_LONG).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        // Handle unsuccessful uploads
+                        // ...
+                    }
+                });
+    }
+
+
+
+    private void Filechoser(){
+        Intent intent=new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityIfNeeded(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==1 && resultCode==RESULT_OK && data!=null && data.getData()!=null){
+            imguri=data.getData();
+            img.setImageURI(imguri);
+        }
     }
 
     public void personalTrainer(personalTrainer personal_trainer){
--- app/src/main/java/com/example/myapplication/content_foto_profil_trainer.java
+++ app/src/main/java/com/example/myapplication/content_foto_profil_trainer.java
@@ -1,14 +1,43 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.webkit.MimeTypeMap;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.view.View;
+import android.widget.Toast;
+
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
 
 public class content_foto_profil_trainer extends AppCompatActivity {
+
+    Button ch,up;
+    ImageView img;
+    StorageReference mStorageReference;
+    public Uri imguri;
+    private StorageTask uploadTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_content_foto_profil_trainer);
+
+
+
     }
+
+
 }
